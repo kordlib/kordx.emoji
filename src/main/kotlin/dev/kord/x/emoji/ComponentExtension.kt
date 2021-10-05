06c41f9cc6e6c7aa17533dc74470077e2df6c3fd
Michael Rittmeister <michael@rittmeister.in>
2021-10-05 13:52:16 +0200

    Add kordx.emoji support to Kord Button Builders

---
+++ src/main/kotlin/dev/kord/x/emoji/ComponentExtension.kt
@@ -0,0 +1,26 @@
+package dev.kord.x.emoji
+
+import dev.kord.common.annotation.KordPreview
+import dev.kord.common.entity.DiscordPartialEmoji
+import dev.kord.rest.builder.component.ButtonBuilder
+
+/**
+ * Allows you to set [ButtonBuilder.emoji] to a [DiscordEmoji].
+ *
+ * Example:
+ * ```kotlin
+ * interactionButton(ButtonStyle.Primary, "id") {
+ *  discordEmoji = Emojis.checkMark
+ * }
+ * ```
+ *
+ * @see ButtonBuilder.emoji
+ */
+@KordPreview
+var ButtonBuilder.discordEmoji: DiscordEmoji?
+    get() = emoji?.name?.let { unicode ->
+        Emojis[unicode]
+    }
+    set(value) {
+        emoji = value?.let { DiscordPartialEmoji(name = it.unicode) }
+    }
